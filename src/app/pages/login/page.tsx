8e63f4f7f9d876c67155773c20377a083ce15522
AnderMichael <andercaymi@gmail.com>
2023-08-10 10:34:51 -0400

    EmptyProject

---
+++ src/app/pages/login/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+export const Login = () => {
+  return (
+    <div
+      className="flex flex-1 justify-center items-center inset-0 absolute"
+    >
+      <h1>Login Page</h1>
+    </div>
+  );
+};
+
+export default Login;
