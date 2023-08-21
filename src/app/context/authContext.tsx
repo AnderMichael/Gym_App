02c050ebc98f626c1ed855fd6dd8029e9653f6e5
Rene Rengel <Rene.Rengel@jala.university>
2023-08-21 16:19:23 -0400

    Add local storage

--- src/app/context/authContext.tsx
+++ src/app/context/authContext.tsx
@@ -1,40 +1,48 @@
 "use client";
-import { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 
 interface AuthContextType {
-    isLogged: boolean;
-    login: () => void;
-    logout: () => void;
+  isLogged: boolean;
+  login: () => void;
+  logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const useAuth = (): AuthContextType => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error('useAuth debe ser usado dentro de un AuthProvider');
-    }
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth debe ser usado dentro de un AuthProvider');
+  }
+  return context;
 };
 
 interface AuthProviderProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
-    const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState<boolean>(() => {
+    // Retrieve authentication status from localStorage on component initialization
+    return JSON.parse(localStorage.getItem('isLogged') || 'false');
+  });
 
-    const login = () => {
-        setIsLogged(true);
-    };
+  const login = () => {
+    setIsLogged(true);
+  };
 
-    const logout = () => {
-        setIsLogged(false);
-    };
+  const logout = () => {
+    setIsLogged(false);
+  };
 
-    return (
-        <AuthContext.Provider value={{ isLogged, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  // Update localStorage when isLogged changes
+  useEffect(() => {
+    localStorage.setItem('isLogged', JSON.stringify(isLogged));
+  }, [isLogged]);
+
+  return (
+    <AuthContext.Provider value={{ isLogged, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
