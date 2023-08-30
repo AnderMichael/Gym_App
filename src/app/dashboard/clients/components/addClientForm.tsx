d3535f8a3552810bb9610b4ad74c02043d36abdf
Rene Rengel <Rene.Rengel@jala.university>
2023-08-29 21:22:32 -0400

    AddClientPagesAndComponents

---
+++ src/app/dashboard/clients/components/addClientForm.tsx
@@ -0,0 +1,158 @@
+"use client";
+import Button from "@/components/Button";
+import useAxios from "axios-hooks";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { toast } from "react-toastify";
+
+const ClientForm = () => {
+  // ! Hooks para el form
+  const router = useRouter(); // NOTE: Para redirigir paginas
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm(); // NOTE: Para hacer validaciones en el formulario
+
+  const [, executePost] = useAxios(
+    {
+      url: "http://localhost:3000/clients",
+      method: "POST",
+    },
+    { manual: true }
+  );
+
+  const onSubmitForm = async (data: any) => {
+    if (isCancel) {
+      try {
+        await executePost({
+          data: {
+            clientFirstName: data.clientFirstName,
+            clientLastName: data.clientLastName,
+            createdDate: data.createdDate,
+            planType: data.planType
+          },
+        });
+        router.push("/dashboard/clients?added");
+      } catch (error) {
+        router.push("/dashboard/clients?error");
+        console.error("Hubo un error al enviar los datos:", error);
+      }
+    }
+  };
+
+  const [isCancel, setIsCancel] = useState(true);
+
+  const registration = () => {
+    setIsCancel(true);
+  };
+
+  const cancellation = () => {
+    setIsCancel(false);
+    router.back();
+  };
+
+  return (
+    <div className="flex bg-[#DC6000] p-10 rounded-md">
+      <form
+        className="flex flex-col space-y-4 md:space-y-6 w-[400px]"
+        onSubmit={handleSubmit(onSubmitForm)}
+      >
+        <div className="flex flex-col">
+          <div className="mb-2 flex-col flex">
+            <label className="text-white">Nombre Completo</label>
+            <input
+              className="bg-white text-gray-800 rounded-lg text-center h-10"
+              placeholder="Panquesito del Castillo Vainilla"
+              type="text"
+              {...register("name", {
+                required: true,
+                minLength: 6,
+                maxLength: 50,
+                pattern: /^[A-Z][a-zA-Z\s]*$/,
+              })}
+            />
+            {errors.name?.type === "required" && (
+              <p className=" text-red-700 font-light leading-relaxed">
+                * Debes introducir un nombre
+              </p>
+            )}
+            {errors.name?.type === "minLength" && (
+              <p className=" text-red-700 font-light leading-relaxed">
+                * El nombre debe ser tener 6 caracteres como mínimo
+              </p>
+            )}
+            {errors.name?.type === "maxLength" && (
+              <p className=" text-red-700 font-light leading-relaxed">
+                * El nombre debe tener 50 caracteres como máximo
+              </p>
+            )}
+            {errors.name?.type === "pattern" && (
+              <p className=" text-red-700 font-light leading-relaxed">
+                * Este no es un nombre válido, nombres con mayúsculas
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="mb-2 flex flex-col">
+            <label className="text-white">Cargo</label>
+            <select
+              className="bg-white text-gray-800 rounded-lg text-center h-10"
+              placeholder="Cargo"
+              {...register("charge")}
+            >
+              <option value="Entrenador">Entrenador</option>
+              <option value="Conserje">Conserje</option>
+            </select>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="mb-2 flex-col flex">
+            <label className="text-white">Numero de Contacto</label>
+            <input
+              className="bg-white text-gray-800 rounded-lg text-center h-10"
+              placeholder="66666666"
+              type="text"
+              {...register("contact", {
+                required: true,
+                pattern: /^[1-9][0-9]{7}$/,
+              })}
+            />
+            {errors.contact?.type === "required" && (
+              <p className="text-red-700 font-light leading-relaxed">
+                * Debes introducir un número de télefono
+              </p>
+            )}
+            {errors.contact?.type === "pattern" && (
+              <p className="text-red-700 font-light leading-relaxed">
+                * Este no es un número de teléfono
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="flex justify-between">
+          <div className="flex flex-1 mx-1">
+            <button
+              className="flex-1 bg-[#1AC317] p-2 text-white rounded-xl hover:bg-[#246623] "
+              onClick={registration}
+            >
+              <h1 className="font-semibold">Registrar</h1>
+            </button>
+          </div>
+          <div className="flex flex-1 mx-1">
+            <button
+              className="flex-1 bg-[#CE0A0B] p-2 text-white rounded-xl hover:bg-[#782828] "
+              onClick={cancellation}
+            >
+              <h1 className="font-semibold">Cancelar</h1>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ClientForm;
---
+++ src/app/dashboard/clients/page.tsx
@@ -0,0 +1,181 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import useAxios from "axios-hooks";
+import { EyeIcon, PlusCircleIcon, TrashIcon } from "@heroicons/react/solid";
+import { useRouter } from "next/navigation";
+import DeleteModal from "./components/DeleteModal";
+import { toast } from "react-toastify";
+import {Button, colors} from '@mui/material';
+import { makeStyles } from '@mui//material/styles';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Card from '@mui/material/Card';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: '#1F1C53',
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+
+
+const styles = {
+  orangeButton: {
+    backgroundColor: 'orange',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: 'darkorange',
+    },
+  },
+}
+
+const ClientsPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);
+    if (queryParams.has("added")) {
+      queryParams.delete("added");
+      window.history.replaceState(
+        {},
+        document.title,
+        `${window.location.pathname}${queryParams}`
+      );
+      sessionStorage.setItem("notification", "added");
+      window.location.reload();
+    } else {
+      if (queryParams.has("error")) {
+        queryParams.delete("error");
+        window.history.replaceState(
+          {},
+          document.title,
+          `${window.location.pathname}${queryParams}`
+        );
+        toast.error("Cliente no añadido", { autoClose: 3000 });
+      }
+    }
+    window.onload = () => {
+      let notification = sessionStorage.getItem("notification");
+      if (notification != null) {
+        if (notification === "added") {
+          toast.success("Cliente agregado exitosamente", {
+            autoClose: 3000,
+            position: "bottom-right",
+            theme: "colored",
+            style: {fontFamily: "inherit"},
+          });
+        } else if (notification === "deleted") {
+          toast.success("Cliente eliminado exitosamente", {
+            autoClose: 3000,
+            position: "bottom-right",
+            theme: "colored",
+            style: {fontFamily: "inherit"},
+          });
+        }
+        sessionStorage.removeItem("notification");
+      }
+    }
+    }, []);
+
+  const [{ data: clientData, loading, error }, refetch] = useAxios(
+    "http://localhost:3000/clients"
+  );
+  const [clientToDelete, setClientToDelete] = useState(null);
+
+  const handleAddClient = () => {
+    router.push("/dashboard/clients/add_client");
+  };
+
+  const handleView = (client: any) => {
+    console.log("Viewing:", client);
+    router.push(`/dashboard/clients/see_client/${client.id}`);
+  };
+
+  const promptToDelete = (client: any) => {
+    setClientToDelete(client);
+  };
+
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error al cargar los datos.</p>;
+
+  interface Client {
+    id: number;
+    clientFirstName: string;
+    clientLastName: string;
+    createdDate: string;
+    planType: string;
+  }
+
+  const sortByClientName = (clients: Client[]): Client[] => {
+    return clients
+      .slice()
+      .sort((a, b) => a.clientFirstName.localeCompare(b.clientFirstName));
+  };
+
+  const sortedClientsData = sortByClientName(clientData);
+
+  return (
+    <>
+      <div className="fixed top-0 bottom-0 left-0 right-0 bg-[#F2F2F2] p-4" >
+        <div className="flex justify-between items-center">
+          <h1 className="text-[#000000] font-extrabold text-4xl ">Clientes</h1>
+          
+          <Button onClick={handleAddClient} variant="contained" style={styles.orangeButton}>Añadir cliente</Button>
+        </div>
+        
+        <TableContainer component={Card}>
+            <Table sx={{ minWidth: 700 }} aria-label="customized table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell>Nombres</StyledTableCell>
+                  <StyledTableCell align="right">Apellidos</StyledTableCell>
+                  <StyledTableCell align="right">Fecha de registro</StyledTableCell>
+                  <StyledTableCell align="right">Plan</StyledTableCell>
+                  <StyledTableCell align="right">Acion(temporal)</StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {sortedClientsData.map((row) => (
+                  <StyledTableRow key={row.id}>
+                    <StyledTableCell component="th" scope="row">
+                      {row.clientFirstName}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">{row.clientLastName}</StyledTableCell>
+                    <StyledTableCell align="right">{row.createdDate}</StyledTableCell>
+                    <StyledTableCell align="right">{row.planType}</StyledTableCell>
+                    
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+      </TableContainer>
+      </div>
+      <DeleteModal
+        isOpen={!!clientToDelete}
+        onClose={() => setClientToDelete(null)}
+        client={clientToDelete}
+      />
+    </>
+  );
+};
+
+export default ClientsPage;
