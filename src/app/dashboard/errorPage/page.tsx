a5263f625f7b8a4a254aebbb7b76a0d0ffb17cc4
Rene Rengel <Rene.Rengel@jala.university>
2023-09-06 11:39:21 -0400

    improveErrorPage

--- src/app/dashboard/errorPage/page.tsx
+++ src/app/dashboard/errorPage/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import Image from "next/image";
+import sadMichi from "../../../assets/sadMichi2.png"
 
 const ErrorPage = () => {
   return (
     <div className="flex flex-col inset-0 justify-center items-center absolute font-jost bg-[#F2F2F2]">
-      <h1>Error
-      </h1>
+        <Image src={sadMichi} alt={"uwu"}/>
+        <p className="text-5xl text-gray-900 dark:text-white mb-4">Oops! Parece que algo salio mal....</p>
+        <p className="text-3xl text-gray-900 dark:text-white mt-4">Verifica tu conexion a internet o intentalo mas tarde</p>
     </div>
   );
 };
