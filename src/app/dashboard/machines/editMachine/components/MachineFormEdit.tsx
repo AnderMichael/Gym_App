21f1ddd6f509b4041b47c4d285b6b597424ffd6f
Rene Rengel <Rene.Rengel@jala.university>
2023-09-13 12:24:37 -0400

    AddEditForm

---
+++ src/app/dashboard/machines/editMachine/components/MachineFormEdit.tsx
@@ -0,0 +1,105 @@
+"use client";
+import Button from "@/components/Button";
+import useAxios from "axios-hooks";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import NameInput from "./form_components/NameInput";
+import BrandInput from "./form_components/BrandInput";
+import NumberInput from "./form_components/NumberInput";
+import DateInput from "./form_components/AcquisitionInput";
+import CheckBoxInput from "./form_components/CheckBoxInput";
+import MaintenanceInput from "./form_components/MaintenanceInput";
+
+
+interface EditFormProps {
+    machineData: any;
+  }
+
+const MachineFormEdit = ({ machineData }: EditFormProps) => {
+  // ! Hooks para el form
+  const router = useRouter(); // NOTE: Para redirigir paginas
+  const [isCancel, setIsCancel] = useState(true);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm(); // NOTE: Para hacer validaciones en el formulario
+
+  const [, updateMachine] = useAxios(
+    {
+      url: `http://localhost:3000/machine/${machineData.id}`,
+      method: "PATCH",
+    },
+    { manual: true }
+  );
+
+  const onSubmitForm = async (data: any) => {
+    console.log(data);
+    if (isCancel) {
+      try {
+        await updateMachine({
+          data: {
+            machineName: data.name,
+            needMaintenance: data.maintenance,
+            machineBrand: data.brand,
+            maintenanceDate: data.maintenance_date,
+          },
+        });
+        router.replace("/dashboard/machines?edited");
+      } catch (error) {
+        router.replace("/dashboard/machines?error");
+        console.error("Hubo un error al enviar los datos:", error);
+      }
+    }
+  };
+
+  const registration = () => {
+    setIsCancel(true);
+  };
+
+  return (
+    <div className="flex bg-white p-10 rounded-md shadow-md shadow-[#C0C0C0] w-[75%]">
+      <form
+        className="flex flex-1 flex-col space-y-4 md:space-y-6 justify-center"
+        onSubmit={handleSubmit(onSubmitForm)}
+      >
+        <h1 className="text-[#302E46] my-3 text-left text-3xl font-black font-jost">
+          Información de la Máquina
+        </h1>
+        <div className="flex flex-col">
+          <div className="flex space-x-4">
+            <NameInput register={register} errors={errors} />
+            <DateInput
+              register={register}
+              errors={errors}
+            />
+          </div>
+          <div className="flex space-x-4">
+            <BrandInput register={register} />
+            <NumberInput register={register} errors={errors} />
+          </div>
+        </div>
+        <h1 className="text-[#302E46] my-3 text-left text-3xl font-black font-jost">
+          Mantenimiento
+        </h1>
+        <div className="flex space-x-4">
+          <CheckBoxInput register={register} />
+          <MaintenanceInput
+            register={register}
+            errors={errors}
+          />
+        </div>
+        <div className="flex">
+          <Button
+            color="bg-[#3A7E3D]"
+            title="Actualizar"
+            onClick={registration}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default MachineFormEdit;
---
+++ src/app/dashboard/machines/editMachine/components/form_components/NameInput.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+interface NameInputProps {
+  register: any;
+  errors: any;
+}
+
+const NameInput: React.FC<NameInputProps> = ({ register }) => {
+  return (
+    <div className="flex-col flex mb-2 flex-1">
+      <label className="text-[#302E46] font-semibold text-xl font-jost">
+        Nombre
+      </label>
+      <select
+        className="bg-white text-gray-800 rounded-lg text-center h-10"
+        {...register("name")}
+      >
+        <option value="Trotadora">Trotadora</option>
+        <option value="Maquina Smith">Maquina Smith</option>
+        <option value="Maquina de poleas multifuncional">
+          Maquina de poleas multifuncional
+        </option>
+        <option value="Bici Eliptica">Bici Eliptica</option>
+        <option value="Prensa de pierna">Prensa de pierna</option>
+        <option value="Maquina de press banca">Maquina de press banca</option>
+      </select>
+    </div>
+  );
+};
+
+export default NameInput;
