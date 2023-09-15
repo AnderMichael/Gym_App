5992518e7af78b739353fd7ab97eb213b1864d24
Rene Rengel <Rene.Rengel@jala.university>
2023-09-14 23:08:11 -0400

    AddUnitTestsDependencies

---
+++ src/test/Test1.test.tsx
@@ -0,0 +1,8 @@
+
+import { render } from '@testing-library/react';
+import MachinesPage from "../app/dashboard/machines/page";
+import { columnsMachines, rowsMachines } from "@/app/helpers/dataColumns";
+
+test('renders without errors', () => {
+    
+});
